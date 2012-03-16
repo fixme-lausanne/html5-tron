d814284c66e9bd97399d3a31abd6bfe08d4307bc
gcmalloc <gcmalloc@gmail.com>
2012-03-16 21:13:52 +0100

    path is ready :)

--- js/main.js
+++ js/main.js
@@ -2,8 +2,17 @@ var canva
 
 var moto = new Image()
 
-function init(){
 
+function init() {
+    
+}
+
+#https://developer.mozilla.org/en/Drawing_Graphics_with_Canvas#Using_Paths
+function drawPath(path) {
+    ctx.beginPath();  
+    for (i in path):
+        ctx.lineTo(i[0], i[1])
+    ctx.stroke();
 }
 
 function mainLoop() {
