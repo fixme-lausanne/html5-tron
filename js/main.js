8de4227e5b16eac690eaf91d49ec4ce1317b9bce
prosouth <Prosouth@62-220-135-213.dhcp.fixme.ch>
2012-03-17 00:22:14 +0100

    draw Sprite 0,1,2,3 positions

--- js/main.js
+++ js/main.js
@@ -5,10 +5,14 @@ var screenX;
 var screenY;
 
 var mainLoopDelay = 10;
-var motoOwnSpriteVert = new Image();
-var motoOwnSpriteHor = new Image();
-var motoOtherSpriteVer = new Image();
-var motoOtherSpriteHor = new Image();
+var motoOwnSprite0 = new Image();
+var motoOwnSprite1 = new Image();
+var motoOwnSprite2 = new Image();
+var motoOwnSprite3 = new Image();
+var motoOtherSprite0 = new Image();
+var motoOtherSprite1 = new Image();
+var motoOtherSprite2 = new Image();
+var motoOtherSprite3 = new Image();
 
 function init() {
     //add event handler for clicking on start/stop button and toggle the game play
@@ -19,10 +23,18 @@ function init() {
 }
 
 function loadImage() {
-    motoOwnSpriteVert.src = "image/tron_blue_vertical.png"
-    motoOwnSpriteHor.src = "image/tron_blue_horizontal.png"
-    motoOtherSpriteVer.src = "image/tron_yellow_vertical.png"
-    motoOtherSpriteHor.src = "image/tron_yellow_horizontal.png"
+    motoOwnSprite = new Array(4);
+    motoOtherSprite = new Array(4);
+    for ( var i = 0;i < 4;i++) {
+		
+    motoOwnSprite0.src = "image/tron_blue_0.png"
+    motoOwnSprite1.src = "image/tron_blue_1.png"
+    motoOwnSprite2.src = "image/tron_blue_2.png"
+    motoOwnSprite3.src = "image/tron_blue_3.png"
+    motoOtherSprite0.src = "image/tron_yellow_0.png"
+    motoOtherSprite1.src = "image/tron_yellow_1.png"
+    motoOtherSprite2.src = "image/tron_yellow_2.png"
+    motoOtherSprite2.src = "image/tron_yellow_3.png
 }
 //https://developer.mozilla.org/en/Drawing_Graphics_with_Canvas#Using_Paths
 function drawPath(path) {
@@ -32,12 +44,20 @@ function drawPath(path) {
     }
     ctx.stroke();
 }
-function drawMotoBlue1(x, y) 
+function drawMotoBlue0(x, y, rot) 
 {
-	ctx.drawImage(motoOwnSpriteVert, x, y)
-
-	
+	switch (rot) {
+	case 0: 
+		ctx.drawImage(motoOwnSprite0, x, y);
+		break;
+	case 1: 
+		ctx.drawImage(motoOwnSprite1, x, y);
+		break;
+	case 2:
+		ctx.drawImage(motoOwnSprite2, x, y)
+		
 }
+
 
 
 function mainLoop() {
