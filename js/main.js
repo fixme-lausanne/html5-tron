4bb41d4dcd21b4522baec1a1c3369246a8813df6
gcmalloc <gcmalloc@gmail.com>
2012-03-16 21:33:12 +0100

    40 % of main loop

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
-var canva
+var ctx
+var gameloopId
+var gameRunning = false
 
+var mainLoopDelay = 10
 var moto = new Image()
 
 
 function init() {
-    
+    #grab the canvas
+    ctx = document.getElementById('canvas').getContext('2d'); 
+
 }
 
 #https://developer.mozilla.org/en/Drawing_Graphics_with_Canvas#Using_Paths
@@ -17,4 +22,20 @@ function drawPath(path) {
 
 function mainLoop() {
     
+}
+
+//Start/stop the game loop (and more importantly that annoying boinging!)
+function toggleGameplay()
+{
+    gameRunning = !gameRunning;
+    
+    if(gameRunning)
+    {
+        clearInterval(gameloopId);
+        gameloopId = setInterval(mainLoop, mainLoopDelay);
+    }
+    else
+    {
+        clearInterval(gameloopId);
+    }
 }
